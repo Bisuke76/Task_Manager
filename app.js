7868d5a1e99ecb640fbed24e98c276789c840b98
Bisuke76 <axel.burguiere76@gmail.com>
2023-07-09 04:32:20 +0200

    app.js finished + style.css created

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
 const taskForm = document.getElementById("task-form");
 const taskList = document.getElementById("task-list");
 
-console.log(taskForm, taskList);
+// Counter variable to track the task index
+
+let taskIndex = 1;
+
+// console.log(taskForm, taskList);
+
+taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("task-input");
+    const taskText = taskInput.value.trim();
+
+    // console.log(taskText);
+
+    if(taskText !==""){
+        // Create a new Task item
+
+        const taskItem = document.createElement("li");
+        taskItem.classList.add("task-item");
+        taskItem.textContent = `${taskIndex}- ${taskText}`;
+
+        taskItem.addEventListener("click", function () {
+            console.log("completed");
+            this.classList.toggle("completed");
+        })
+        
+        // Append the task item to the task List
+        taskList.appendChild(taskItem);
+
+        //Increment the task index
+
+        taskIndex++;
+
+        taskInput.value = "";
+    }
+});
